f68a6e76962eb90f75e698c09c8b285a1367c19b
Tejas Kalpathi <60577394+TrexKalp@users.noreply.github.com>
2023-09-09 14:35:23 -0500

    basic django app made

--- HowdyHack-23/src/App.jsx
+++ HowdyHack-23/src/App.jsx
@@ -4,8 +4,6 @@ import viteLogo from "/vite.svg";
 import SimpleCard from "./components/SimpleCard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <SimpleCard />
