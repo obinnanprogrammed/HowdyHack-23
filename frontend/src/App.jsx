ab3f0ba219df025cf758743cc35df02dbd8b9c76
obinnanprogrammed <obinnanwakwue10@gmail.com>
2023-09-09 16:54:11 -0500

    added react-icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import SimpleCard from "./components/SimpleCard";
+import SimpleSidebar from "./components/Home"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [data, setData] = useState(null);
@@ -11,7 +13,17 @@ function App() {
     });
   }, []);
 
-  return <div>{data ? <SimpleCard /> : "Loading..."}</div>;
+  // return <div>{data ? <SimpleCard /> : "Loading..."}</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" component={SimpleCard} />
+        <Route path="/home" component = {SimpleSidebar} />
+        
+      </Routes>
+      <SimpleCard />
+    </Router>
+  )
 }
 
 export default App;
