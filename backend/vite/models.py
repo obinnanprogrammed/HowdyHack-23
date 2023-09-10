652b5dfaca6c99c6fdc5b3dc069e65fad2480fef
skandrigi <skandrigi@gmail.com>
2023-09-09 20:17:43 -0500

    database change

--- backend/vite/models.py
+++ backend/vite/models.py
@@ -1,30 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    loyalty_points = models.PositiveIntegerField(default=0)
-
-    def __str__(self):
-        return self.username
-    
 class Receipt(models.Model):
-    person_name = models.CharField("Person Name", max_length=240)
-    business_name = models.CharField("Name of Business", max_length=240)
-    receipt_number = models.CharField("Receipt Number", max_length=100, unique=True)
-    subtotal = models.DecimalField("Subtotal", max_digits=10, decimal_places=2)
-    date = models.DateField("Date")
+    first_name = models.CharField("First Name",max_length=20)
+    last_name = models.CharField("Last Name",max_length=20)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    receipt_file = models.FileField(upload_to='receipts/')
 
     def __str__(self):
-        return f"Receipt {self.receipt_number} from {self.business_name}"
-    
-class RewardPoints(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
-    points_earned = models.PositiveIntegerField()
-    date_earned = models.DateField()
-
-    def __str__(self):
-        return f"{self.user}'s Points for Receipt {self.receipt.receipt_number} on {self.date_earned}"
+        return f"Receipt from {self.first_name} {self.last_name} at {self.uploaded_at}"
--- backend/vite/views.py
+++ backend/vite/views.py
@@ -1,5 +1,13 @@
-from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from .forms import ReceiptUploadForm
 
-
-def my_view(request):
-    return JsonResponse({"message": "Test from Django!"})
+def upload_receipt(request):
+    if request.method == 'POST':
+        form = ReceiptUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Process the receipt here to award points to the customer
+            return redirect('vite:receipt_upload_success')
+    else:
+        form = ReceiptUploadForm()
+    return render(request, 'vite/upload_receipt.html', {'form': form})
